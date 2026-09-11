2487e4c1a8c857b1f4951dbae79280306a980614
Carlos8992 <cvelasco@saludelectronica.com>
2026-09-11 09:51:57 -0500

    feat: cambios en velocidad

--- sql/R__sp_actualizar_pedido.sql
+++ sql/R__sp_actualizar_pedido.sql
@@ -13,6 +13,8 @@ CREATE PROCEDURE sp_actualizar_pedido(IN p_pedido_id INT, IN p_estado VARCHAR(20
 BEGIN
     DECLARE v_estado_actual VARCHAR(20);
     
+
+
     
     IF p_estado NOT IN ('pendiente', 'procesando', 'enviado', 'entregado', 'cancelado', 'devuelto') THEN
         SIGNAL SQLSTATE '45000'
